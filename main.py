ca6400e367b67765ddeb1fef60c32d6282ada91e
joboot <jordan.booth41900@gmail.com>
2021-09-27 22:24:35 -0500

    Initial commit stating inputs, outputs, and header.

---
+++ main.py
@@ -0,0 +1,16 @@
+# Program to calculate and determine frost cracking intensity
+# Programmer: Jordan Booth
+# Date: 2021.09.27
+
+# inputs:
+# MAT (mean annual Temperature)
+# max summer Temp
+# min winter Temp
+# z (depth)
+# change in z (change in depth)
+# dT/dz (change in temp over change in depth)
+# T should stay in between 0 - -15
+
+# outputs:
+# FCI (frost cracking intensity)
+# depth to 0 FCI
