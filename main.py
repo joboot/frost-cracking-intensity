3ff226e12cbd0cc9e8fd65719472ec33e7a32c6e
joboot <jordan.booth41900@gmail.com>
2022-03-23 23:11:29 -0500

    Calculated final FCI and created respective data frames. Split into functions

--- main.py
+++ main.py
@@ -27,83 +27,22 @@ __status__ = "Development"
 def main():
     # mean_annual_temp, max_summer_temp, min_winter_temp, delta_depth, temp_over_depth = inputs()
 
-    # 1D list holding data for each day at each depth
-    data_list = []
-
+    # For testing purposes
     mean_annual_temp = 0.1
     max_summer_temp = 10.9
     min_winter_temp = -10.9
     max_depth = 2000
     delta_depth = 10
-    temp_over_depth = 0
-    depth = 0
-    day = 1
 
-    # List to hold the depths for the indices
-    depths = []
-    # Creates a range between 0-max_depth (Maximum depth) with intervals of delta_depth (Change in depth)
-    for i in range(0, max_depth + 1, delta_depth):
-        depths.append(i)
+    # Main loop to get the data required
+    data_list, depths, days = loop(max_depth, delta_depth, mean_annual_temp, max_summer_temp, min_winter_temp)
 
-    # List to hold the numbered days of the year for the columns
-    days = []
-    # Creates a range between 1-365 for each day of the year
-    for i in range(1, 366):
-        days.append(i)
+    # Create dataframes out of the data
+    ta_dataframe, ccm_dataframe, fci_dataframe, total_fci = \
+        create_dataframes(data_list, max_depth, delta_depth, depths, days)
 
-    # Loop to get the date for all of depth intervals 2000 cm and under
-    while depth <= max_depth:
-
-        # Loop to get the calculations in a year at this depth interval
-        while day <= 365:
-            # Calculate T(z,t) Temperature as a function of depth in bedrock and time
-            # temp = calculations(mean_annual_temp, max_summer_temp, min_winter_temp, depth, day)
-            temp = round(calculations(mean_annual_temp, max_summer_temp, min_winter_temp, depth, day), 2)
-
-            # Add new calculation to the list of data
-            data_list.append(temp)
-
-            """
-            if  0 >= change_in_day[day - 1] <= -15:
-                math.fabs()
-            """
-            day += 1
-
-        # Set the day back to 1 for the next depth
-        day = 1
-
-        # Change the depth to the next depth based on the depth interval
-        depth = depth + delta_depth
-
-    # Creation of a 2D numpy array using the split function to split the entirety data into different rows based
-    # on the maximum depth and each depth interval
-    tz_array = np.array_split(data_list, (max_depth / delta_depth) + 1)
-
-    # Create dataframe for the T/z (Temperature over depth)
-    ta_dataframe = pd.DataFrame(tz_array, index=depths, columns=days)
-    print(ta_dataframe)
-
-""" 
-    test = np.array([-12, -30, -2, 5, 0, -2, -15]
-                    [-24, 7, 81, 2, -6, -9, 10, -13])
-    print(test)
-    condlist = [test > 0, test < -15]
-    choicelist = [0, 0]
-    new_test = np.select(condlist, choicelist, test*10)
-    print(new_test)
-
-    #arr = np.arange(8)
-
-    #print(arr)
-
-    #condlist = [arr < 3, arr > 4]
-    #choicelist = [arr, arr ** 3]
-
-    #gfg = np.select(condlist, choicelist)
-
-    #print(gfg)
-
-"""
+    # Displays dataframes and total FCI
+    display_output(ta_dataframe, ccm_dataframe, fci_dataframe, total_fci)
 
 
 def inputs():
@@ -136,12 +75,105 @@ def calculations(mean_annual_temp, max_summer_temp, min_winter_temp, depth, day)
     return temp
 
 
-def check_temp(temps):
-    print("check temps")
+def create_dataframes(data_list, max_depth, delta_depth, depths, days):
+    # Creation of a 2D numpy array using the split function to split the entirety data into different rows based
+    # on the maximum depth and each depth interval
+    tz_array = np.array_split(data_list, (max_depth / delta_depth) + 1)
+
+    # Create dataframe for the T/z (Temperature over depth)
+    ta_dataframe = pd.DataFrame(tz_array, index=depths, columns=days)
+
+    # Create a numpy array from the initial data list
+    ccm_array = np.array(data_list)
+
+    # Variable for the index of the current element
+    index = 0
+
+    # np.nditer function to iterate through the numpy array
+    # Uses the optional flag 'readwrite' to change the array based on a if/else statement
+    for i in np.nditer(ccm_array, op_flags=['readwrite']):
+        # If the temperatures are between 0 and -15
+        if 0 >= i >= -15:
+            # Variable for the the next depth on the same day
+            j = ccm_array[index + 365]
+            # Celsius/centimeter equation
+            i[...] = math.fabs((j - i)/delta_depth)
+        else:
+            # If not between 0 and -15, make it 0
+            i[...] = 0
+
+        # Increment index
+        index += 1
+
+    # Split the array into years at certain depths
+    ccm_array = np.array_split(ccm_array, (max_depth / delta_depth) + 1)
+    # Create Celsius/centimeter dataframe based on that array
+    ccm_dataframe = pd.DataFrame(ccm_array, index=depths, columns=days)
+
+    # Dataframe of the FCI at each depth
+    fci_dataframe = ccm_dataframe["FCI"] = ccm_dataframe.sum(axis=1)
+
+    # Total FCI for the entire year at all depths
+    total_fci = fci_dataframe.sum(axis=0)
+
+    return ta_dataframe, ccm_dataframe, fci_dataframe, total_fci
 
 
-def output():
-    print('Output: ')
+def display_output(ta_dataframe, ccm_dataframe, fci_dataframe, total_fci):
+    # Display array while rounding to 2 decimal places
+    print(ta_dataframe.round(2))
+
+    # Display array while rounding to 2 decimal places
+    print(ccm_dataframe.round(2))
+
+    # Display array while rounding to 2 decimal places
+    print(fci_dataframe.round(2))
+
+    print()
+    print("Total FCI:", round(total_fci))
+
+
+def loop(max_depth, delta_depth, mean_annual_temp, max_summer_temp, min_winter_temp):
+
+    depth = 0
+    day = 1
+
+    # 1D list holding data for each day at each depth
+    data_list = []
+
+    # List to hold the depths for the indices
+    depths = []
+    # Creates a range between 0-max_depth (Maximum depth) with intervals of delta_depth (Change in depth)
+    for i in range(0, max_depth + 1, delta_depth):
+        depths.append(i)
+
+    # List to hold the numbered days of the year for the columns
+    days = []
+    # Creates a range between 1-365 for each day of the year
+    for i in range(1, 366):
+        days.append(i)
+
+    # Loop to get the date for all of depth intervals 2000 cm and under
+    while depth <= max_depth:
+
+        # Loop to get the calculations in a year at this depth interval
+        while day <= 365:
+            # Calculate T(z,t) Temperature as a function of depth in bedrock and time
+            # temp = calculations(mean_annual_temp, max_summer_temp, min_winter_temp, depth, day)
+            temp = calculations(mean_annual_temp, max_summer_temp, min_winter_temp, depth, day)
+
+            # Add new calculation to the list of data
+            data_list.append(temp)
+
+            day += 1
+
+        # Set the day back to 1 for the next depth
+        day = 1
+
+        # Change the depth to the next depth based on the depth interval
+        depth = depth + delta_depth
+
+    return data_list, depths, days
 
 
 if __name__ == "__main__":
