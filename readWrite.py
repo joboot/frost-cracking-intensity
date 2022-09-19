852da6d9f99c536c75cda6d224e52928f1b17295
joboot <jordan.booth41900@gmail.com>
2022-09-18 22:43:52 -0500

    Made accessible from the GUI.py

--- readWrite.py
+++ readWrite.py
@@ -1,29 +1,41 @@
 import xlsxwriter
 import xlwings as xw
 import calculator
+import pandas as pd
 
 
 def main():
     print("readwrite main")
 
 
-def write_to_excel():
+def write_to_excel(dataframe, entries):
     print("writeToExcel")
+    # First method, not awful
+    # =========================================================================
+
     # book = xw.Book()
     # sheet = book.sheets[0]
     # sheet['A1'].value = "MAT"
     #
-    # sheet['D1'].value = main.ta_dataframe
-    #
-    # writer = pd.ExcelWriter('output.xlsx', engine='xlsxwriter')
-    # # writer.write_cells("A1", )
-    # # writer.write_cells("A2", mean_annual_temp)
-    # #
-    # workbook = writer.book
-    # # ta_dataframe.to_excel(writer, float_format='%.2f', startrow=4, startcol=0)
-    # fci_dataframe.to_excel(writer, startrow=4, startcol=0)
-    # writer.save()
+    # sheet['D1'].value = dataframe
 
+    # =========================================================================
+    #
+    # =========================================================================
+
+    writer = pd.ExcelWriter('output.xlsx', engine='xlsxwriter')
+    # writer.write_cells("A1", "MAT")
+    # writer.write_cells("A2", entries[0])
+
+    # ta_dataframe.to_excel(writer, float_format='%.2f', startrow=4, startcol=0)
+    dataframe.to_excel(writer, startrow=4, startcol=0)
+    writer.save()
+
+    # =========================================================================
+    #
+    # =========================================================================
+
+    # workbook = writer.book
     # workbook = xlsxwriter.Workbook('output.xlsx')
     # worksheet = workbook.add_worksheet()
     # worksheet.write_string(0, 0, "MAT")
