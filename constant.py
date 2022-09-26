dcec50122145a16d9558a6bfec8ac3aa2451ada1
joboot <jordan.booth41900@gmail.com>
2022-09-25 22:43:55 -0500

    New constants and test main

--- constant.py
+++ constant.py
@@ -1,3 +1,17 @@
-CANVAS_HEIGHT = 500
-CANVAS_WIDTH = 500
+CANVAS_HEIGHT = 800
+CANVAS_WIDTH = 800
 degree_symbol = u'\N{DEGREE SIGN}'
+MAX_FCI_DEPTH = 5000
+alpha_unit = u'cm\u00b2 day\u207B\u00b2'
+ta = u'T\u2090'
+fci_unit = degree_symbol + 'C cm\u207B\u00b9 year\u207B\u00b9'
+
+
+def main():
+    print(alpha_unit)
+    print(ta)
+    print(fci_unit)
+
+
+if __name__ == "__main__":
+    main()
