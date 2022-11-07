404c5172206bed5810e3d8be15ef543e0ddba74b
joboot <jordan.booth41900@gmail.com>
2022-11-06 21:19:57 -0600

    Added standardized FCI 10 0-15 display

--- readWrite.py
+++ readWrite.py
@@ -10,10 +10,10 @@ def main():
     print("readwrite main")
 
 
-def write_to_excel(dataframe, entries, depth_to_0):
+def write_to_excel(fci_dataframe, entries, depth_to_0, total_fci, fci_dataframe_fci_10015, depth_to_0_fci_10015, total_fci_fci_10015):
     print("writeToExcel")
     mat = entries[0]
-    ta = (entries[1]-entries[2])/2
+    ta = (entries[1]-entries[2])/4
     max_depth = entries[3]
     depth_interval = entries[4]
     thermal_diffusivity = entries[5]
@@ -32,38 +32,56 @@ def write_to_excel(dataframe, entries, depth_to_0):
         data_ex_headers_range.api.Borders(border_id).Weight = 2
         data_ex_headers_range.api.Borders(border_id).Color = 0xFFFFFF
 
-    sheet['A1'].value = "MAT (" + constant.degree_symbol + "C)"
-    sheet['A2'].value = mat
+    sheet['A1'].value = "Total FCI (" + constant.fci_unit + ")"
+    sheet['A2'].value = total_fci
 
-    sheet['B1'].value = constant.ta + " (" + constant.degree_symbol + "C)"
-    sheet['B2'].value = ta
+    sheet['B1'].value = "Total FCI" + constant.fci_10015_subscript + " (" + constant.fci_unit + ")"
+    sheet['B2'].value = total_fci_fci_10015
 
-    sheet['C1'].value = "Max Depth (cm)"
-    sheet['C2'].value = max_depth
+    sheet['C1'].value = "MAT (" + constant.degree_symbol + "C)"
+    sheet['C2'].value = mat
 
-    sheet['D1'].value = "Depth interval (cm)"
-    sheet['D2'].value = depth_interval
+    sheet['D1'].value = constant.ta + " (" + constant.degree_symbol + "C)"
+    sheet['D2'].value = ta
 
-    sheet['E1'].value = "Thermal Diffusivity of Rock (" + constant.alpha_unit + ")"
-    sheet['E2'].value = thermal_diffusivity
+    sheet['E1'].value = "Max Depth (cm)"
+    sheet['E2'].value = max_depth
 
-    sheet['F1'].value = "Frost Cracking window (" + constant.degree_symbol + "C)"
-    sheet['F2'].value = str(frost_cracking_window_max) + ' - ' + str(frost_cracking_window_min)
+    sheet['F1'].value = "Depth interval (cm)"
+    sheet['F2'].value = depth_interval
 
-    sheet['G1'].value = "Depth to 0 (cm)"
-    sheet['G2'].value = depth_to_0
+    sheet['G1'].value = "Thermal Diffusivity of Rock (" + constant.alpha_unit + ")"
+    sheet['G2'].value = thermal_diffusivity
+
+    sheet['H1'].value = "Frost Cracking window (" + constant.degree_symbol + "C)"
+    sheet['H2'].value = str(frost_cracking_window_max) + ' - ' + str(frost_cracking_window_min)
+
+    sheet['I1'].value = "Depth to 0 FCI (cm)"
+    sheet['I2'].value = depth_to_0
+
+    sheet['J1'].value = "Depth to 0 FCI" + constant.fci_10015_subscript + " (cm)"
+    sheet['J2'].value = depth_to_0_fci_10015
 
     sheet['A5'].value = "Depth (cm)"
     sheet['B5'].value = "FCI (" + constant.fci_unit + ")"
-    sheet['A6'].value = dataframe
+    sheet['A6'].value = fci_dataframe
+
+    sheet['D5'].value = "Depth (cm)"
+    sheet['E5'].value = "FCI" + constant.fci_10015_subscript + " (" + constant.fci_unit + ")"
+    sheet['D6'].value = fci_dataframe_fci_10015
 
     sheet.range('B6:B2000').number_format = '0.00'
+    sheet.range('E6:E2000').number_format = '0.00'
+
+    # ------------------------------------------------------------------------------------------
+    # Header Label
+    # ------------------------------------------------------------------------------------------
 
     header_label_range = sheet.range("A1").expand('right')
     header_label_range.column_width = 20
 
-    header_label_range.api.Font.Name = 'Times New Roman'
-    header_label_range.api.Font.Size = 12
+    header_label_range.api.Font.Name = 'Arial'
+    header_label_range.api.Font.Size = 10
     header_label_range.api.Font.Bold = True
     header_label_range.api.WrapText = True
     header_label_range.api.HorizontalAlignment = constants.HAlign.xlHAlignCenter
@@ -71,20 +89,27 @@ def write_to_excel(dataframe, entries, depth_to_0):
     for border_id in range(7, 13):
         header_label_range.api.Borders(border_id).Weight = 2
 
+    # ------------------------------------------------------------------------------------------
+    # Header Data
+    # ------------------------------------------------------------------------------------------
+
     header_data_range = sheet.range("A2").expand('right')
     header_data_range.row_height = 15
     header_data_range.column_width = 20
 
-    header_data_range.api.Font.Name = 'Times New Roman'
-    header_data_range.api.Font.Size = 12
+    header_data_range.api.Font.Name = 'Arial'
+    header_data_range.api.Font.Size = 10
     header_data_range.api.WrapText = True
     header_data_range.api.HorizontalAlignment = constants.HAlign.xlHAlignCenter
 
+    # ------------------------------------------------------------------------------------------
+    # FCI Dataframe Header
+    # ------------------------------------------------------------------------------------------
     fci_header_range = sheet.range("A5").expand('right')
     fci_header_range.column_width = 15
 
-    fci_header_range.api.Font.Name = 'Times New Roman'
-    fci_header_range.api.Font.Size = 12
+    fci_header_range.api.Font.Name = 'Arial'
+    fci_header_range.api.Font.Size = 10
     fci_header_range.api.WrapText = True
     fci_header_range.api.Font.Bold = True
     fci_header_range.api.HorizontalAlignment = constants.HAlign.xlHAlignCenter
@@ -92,79 +117,46 @@ def write_to_excel(dataframe, entries, depth_to_0):
     for border_id in range(7, 13):
         fci_header_range.api.Borders(border_id).Weight = 2
 
+    # ------------------------------------------------------------------------------------------
+    # FCI Dataframe Data
+    # ------------------------------------------------------------------------------------------
+
     fci_data_range = sheet.range("A6").expand('table')
     fci_data_range.row_height = 15
     fci_data_range.column_width = 15
 
-    fci_data_range.api.Font.Name = 'Times New Roman'
-    fci_data_range.api.Font.Size = 12
+    fci_data_range.api.Font.Name = 'Arial'
+    fci_data_range.api.Font.Size = 10
     fci_data_range.api.WrapText = False
     fci_data_range.api.HorizontalAlignment = constants.HAlign.xlHAlignCenter
-    #
-    #
-    # header_range = sheet.range("A1").expand('right')
-    # header_range.color = (112, 173, 71)
-    # header_range.api.Font.Color = 0x000000
-    # header_range.api.Font.Bold = True
-    # header_range.api.Font.Size = 9
 
-    # =========================================================================
-    #
-    # =========================================================================
+    # ------------------------------------------------------------------------------------------
+    # FCI_10015 Dataframe Header
+    # ------------------------------------------------------------------------------------------
+    fci_10015_header_range = sheet.range("D5").expand('right')
+    fci_10015_header_range.column_width = 15
 
-    # file_name = "output.xlsx"
-    # sheet_name = "Output"
-    #
-    # writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
-    # dataframe.to_excel(writer, sheet_name=sheet_name, startrow=4, startcol=0)
-    #
-    # workbook = writer.book
-    # worksheet = writer.sheets[sheet_name]
-    # # worksheet.write(0, 0, 'Cryptocurrency Pricing Summary on ' + datetime.now().strftime('%d %b %Y'),
-    # #                 workbook.add_format({'bold': True, 'color': '#E26B0A', 'size': 14}))
-    #
-    # worksheet.write(0, 2, "MAT (" + constant.degree_symbol + "C)")
-    # worksheet.write(1, 2, mat)
-    # worksheet.set_column(2, 2, 10)
-    #
-    # worksheet.write(0, 3, constant.ta + " (" + constant.degree_symbol + "C)")
-    # worksheet.write(1, 3, ta)
-    # worksheet.set_column(3, 3, 10)
-    #
-    # worksheet.write(0, 4, "Max Depth (cm)")
-    # worksheet.write(1, 4, max_depth)
-    # worksheet.set_column(4, 4, 15)
-    #
-    # worksheet.write(0, 5, "Depth interval (cm)")
-    # worksheet.write(1, 5, depth_interval)
-    # worksheet.set_column(5, 5, 20)
-    #
-    # worksheet.write(0, 6, "Thermal Diffusivity of rock (" + constant.alpha_unit + ")")
-    # worksheet.write(1, 6, thermal_diffusivity)
-    # worksheet.set_column(6, 6, 28)
-    #
-    # worksheet.write(0, 7, "Frost Cracking window (" + constant.degree_symbol + "C)")
-    # worksheet.write(1, 7, str(entries[6]) + ' - ' + str(entries[7]))
-    # worksheet.set_column(7, 7, 25)
-    #
-    # workbook.close()
-    #
-    # # writer.write_cells("A2", entries[0])
-    #
-    # # ta_dataframe.to_excel(writer, float_format='%.2f', startrow=4, startcol=0)
-    # # dataframe.to_excel(writer, )
-    # writer.save()
+    fci_10015_header_range.api.Font.Name = 'Arial'
+    fci_10015_header_range.api.Font.Size = 10
+    fci_10015_header_range.api.WrapText = True
+    fci_10015_header_range.api.Font.Bold = True
+    fci_10015_header_range.api.HorizontalAlignment = constants.HAlign.xlHAlignCenter
 
+    for border_id in range(7, 13):
+        fci_10015_header_range.api.Borders(border_id).Weight = 2
 
-    # =========================================================================
-    #
-    # =========================================================================
+    # ------------------------------------------------------------------------------------------
+    # FCI_10015 Dataframe Data
+    # ------------------------------------------------------------------------------------------
 
-    # workbook = writer.book
-    # workbook = xlsxwriter.Workbook('output.xlsx')
-    # worksheet = workbook.add_worksheet()
-    # worksheet.write_string(0, 0, "MAT")
-    # workbook.close()
+    fci_10015_data_range = sheet.range("D6").expand('table')
+    fci_10015_data_range.row_height = 15
+    fci_10015_data_range.column_width = 15
+
+    fci_10015_data_range.api.Font.Name = 'Arial'
+    fci_10015_data_range.api.Font.Size = 10
+    fci_10015_data_range.api.WrapText = False
+    fci_10015_data_range.api.HorizontalAlignment = constants.HAlign.xlHAlignCenter
 
 
 if __name__ == "__main__":
