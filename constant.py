2a83bb216e36930e6efcc5eb60b6af5cc63bdce3
joboot <jordan.booth41900@gmail.com>
2022-11-06 21:15:18 -0600

    Added standardized FCI 10 0-15 subscript

--- constant.py
+++ constant.py
@@ -5,13 +5,16 @@ MAX_FCI_DEPTH = 5000
 alpha_unit = u'cm\u00b2 day\u207B\u00b9'
 ta = u'T\u2090'
 fci_unit = degree_symbol + 'C cm\u207B\u00b9 year\u207B\u00b9'
+fci_10015_subscript = '\u2081\u2080\u002C\u2080\u208B\u2081\u2085'
 
 
 def main():
     print(alpha_unit)
     print(ta)
     print(fci_unit)
+    print("FCI"+fci_10015_subscript)
 
 
 if __name__ == "__main__":
     main()
+
